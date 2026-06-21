ce26339a1779b8b080709ffe4451b73fe452e02d
Paulllight <my.edutu@gmail.com>
2026-06-21 07:51:33 +0100

    Render AfriAI assistant entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import AfriAIApp from './AfriAIApp.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AfriAIApp />
   </StrictMode>,
 );
