75d01de19487c8f7700ca1a3cdd44d2c32b9708b
Paulllight <my.edutu@gmail.com>
2026-06-21 10:07:25 +0100

    Wire the client entrypoint to the AfriAI assistant

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App.tsx';
+import AfriAIApp from './AfriAIApp.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AfriAIApp />
   </StrictMode>,
 );
 
